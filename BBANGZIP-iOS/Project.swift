5110de8902b31c5367321e65d9817a314aa1faa7
0gonge <Challengef0802@gmail.com>
2025-04-29 16:41:14 +0900

    init: Project 설정 (#1)

--- BBANGZIP-iOS/Project.swift
+++ BBANGZIP-iOS/Project.swift
@@ -1,34 +1,81 @@
 import ProjectDescription
+import ProjectDescriptionHelpers
 
-let project = Project(
-    name: "BBANGZIP-iOS",
-    targets: [
-        .target(
-            name: "BBANGZIP-iOS",
-            destinations: .iOS,
-            product: .app,
-            bundleId: "io.tuist.BBANGZIP-iOS",
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
-                ]
-            ),
-            sources: ["BBANGZIP-iOS/Sources/**"],
-            resources: ["BBANGZIP-iOS/Resources/**"],
-            dependencies: []
-        ),
-        .target(
-            name: "BBANGZIP-iOSTests",
-            destinations: .iOS,
-            product: .unitTests,
-            bundleId: "io.tuist.BBANGZIP-iOSTests",
-            infoPlist: .default,
-            sources: ["BBANGZIP-iOS/Tests/**"],
-            resources: [],
-            dependencies: [.target(name: "BBANGZIP-iOS")]
-        ),
+let infoPlist: [String: Plist.Value] = [
+    "CFBundleDisplayName": "제 과제 빵점",
+    "UIMainStoryboardFile": "",
+    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+    "UIApplicationSceneManifest": [
+        "UIApplicationSupportsMultipleScenes": false,
+        "UISceneConfigurations": [
+            "UIWindowSceneSessionRoleApplication": [
+                [
+                    "UISceneConfigurationName": "Default Configuration",
+                    "UISceneDelegateClassName": "$(PRODUCT_MODULE_NAME).SceneDelegate"
+                ],
+            ]
+        ]
+    ],
+    "CFBundleURLTypes": [
+        [
+            "CFBundleTypeRole": "Editor",
+            "CFBundleURLSchemes": ["kakao$(KAKAO_NATIVE_APP_KEY)"]
+        ],
+    ],
+    "LSApplicationQueriesSchemes": [
+        "kakaokompassauth",
+        "kakaolink",
+        "kakao$(KAKAO_NATIVE_APP_KEY)"
+    ],
+    "KAKAO_NATIVE_APP_KEY": "$(KAKAO_NATIVE_APP_KEY)",
+    "BASE_URL": "$(BASE_URL)",
+    "NSAppTransportSecurity": [
+        "NSAllowsArbitraryLoads": true
+    ],
+    "UIUserInterfaceStyle": "Light",
+    "UISupportedInterfaceOrientations": [
+        "UIInterfaceOrientationPortrait"
+    ]
+]
+
+private let settings = Settings.settings(
+    base: ["SWIFT_VERSION": "6.0"],
+    configurations: [
+        .debug(name: .debug, xcconfig:
+                .relativeToRoot("BBANGZIP/Resources/Config/Secrets.xcconfig")),
+        .release(name: .release, xcconfig: .relativeToRoot("BBANGZIP/Resources/Config/Secrets.xcconfig"))
+    ])
+
+private let moduleName = "BBANGZIP"
+
+let project = Project.makeModule(
+    name: moduleName,
+    destinations: [.iPhone, .iPad],
+    product: .app,
+    bundleId: "BBANGZIP-iOS.app",
+    infoPlist: .extendingDefault(with: infoPlist),
+    sources: ["BBANGZIP/Sources/**"],
+    resources: ["BBANGZIP/Resources/**"],
+    dependencies: [
+        .external(name: "Alamofire", condition: .none),
+        .external(name: "KakaoSDKAuth", condition: .none),
+        .external(name: "KakaoSDKCommon", condition: .none),
+        .external(name: "KakaoSDKUser", condition: .none),
+        .external(name: "Kingfisher", condition: .none),
+        .external(name: "CocoaCombineCocoa", condition: .none)
+    ],
+    settings: settings
+)
+
+let testTarget = Project.makeModule(
+    name: "\(moduleName)Tests",
+    destinations: [.iPhone, .iPad],
+    product: .unitTests,
+    bundleId: "\(moduleName)Tests",
+    infoPlist: .default,
+    sources: ["BBANGZIP/Tests/**"],
+    resources: [],
+    dependencies: [
+        .target(name: moduleName)
     ]
 )
