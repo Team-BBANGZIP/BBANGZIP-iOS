cda3ed168593c7be772837c40900e1092377efbb
0gonge <Challengef0802@gmail.com>
2025-04-29 16:31:29 +0900

    init: Package Setting 설정 (#1)

--- BBANGZIP-iOS/Tuist/Package.swift
+++ BBANGZIP-iOS/Tuist/Package.swift
@@ -1,22 +1,31 @@
 // swift-tools-version: 6.0
+@preconcurrency
 import PackageDescription
 
 #if TUIST
-    import struct ProjectDescription.PackageSettings
+import ProjectDescription
 
-    let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,]
-        productTypes: [:]
-    )
+let packageSettings = PackageSettings(
+    productTypes: [
+        "Alamofire": .framework,
+        "KakaoOpenSDK": .framework,
+        "Kingfisher": .framework,
+        "CombineCocoa": .framework,
+    ]
+)
 #endif
 
+let kakaoVersion = "2.23.0"
+let alamofireVersion = "5.10.2"
+let kingfisherVersion = "8.1.3"
+let cocoapodsVersion = "0.4.1"
+
 let package = Package(
-    name: "BBANGZIP-iOS",
+    name: "BBANGZIP",
     dependencies: [
-        // Add your own dependencies here:
-        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
-        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+        .package(url: "https://github.com/kakao/kakao-ios-sdk", .upToNextMajor(from: Version(kakaoVersion)!)),
+        .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: Version(alamofireVersion)!)),
+        .package(url: "https://github.com/onevcat/Kingfisher", .upToNextMajor(from: Version(kingfisherVersion)!)),
+        .package(url: "https://github.com/CombineCommunity/CombineCocoa.git", .upToNextMajor(from: Version(cocoapodsVersion)!))
     ]
 )
