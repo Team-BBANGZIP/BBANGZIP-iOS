736915990d0268f22dc5084e9871c860b511a342
hongseekim <songhee0617@gmail.com>
2025-05-13 16:44:44 +0900

    feat: Checkbox 컴포넌트 구현

---
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Checkbox.swift
@@ -0,0 +1,38 @@
+//
+//  Checkbox.swift
+//  BBANGZIP
+//
+//  Created by 김송희 on 5/13/25.
+//
+
+import SwiftUI
+
+struct Checkbox: View {
+    @State private var isChecked: Bool = false
+    
+    var body: some View {
+        Button(action: {
+            isChecked.toggle()
+        }) {
+            ZStack {
+                Image(.icBread)
+                    .renderingMode(.template)
+                    .resizable()
+                    .frame(width: 19, height: 19)
+                    .foregroundColor(isChecked ? Color(.todored1) : Color(.secondaryNormal))
+                
+                if isChecked {
+                    Image(.icCheck)
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: 9, height: 9)
+                        .foregroundColor(Color(.staticwhite))
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    Checkbox()
+}
