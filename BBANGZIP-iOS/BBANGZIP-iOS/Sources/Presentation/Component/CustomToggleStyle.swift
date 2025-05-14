c1dd979c6572b38e64d1ec404065cbcbf1cbcefb
ChoiYouBin <cub8533@naver.com>
2025-05-14 15:47:05 +0900

    feat: CustomToggleStyle 구현 (#7)

---
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Presentation/Component/CustomToggleStyle.swift
@@ -0,0 +1,76 @@
+//
+//  ToggleButton.swift
+//  BBANGZIP
+//
+//  Created by 최유빈 on 5/13/25.
+//
+
+import SwiftUI
+
+struct CustomToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack(alignment: configuration.isOn ? .trailing : .leading) {
+            RoundedRectangle(cornerRadius: 15)
+                .frame(
+                    width: 96,
+                    height: 28
+                )
+                .foregroundColor(Color(.secondaryStrong))
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(
+                        width: 47,
+                        height: 26
+                    )
+                    .foregroundColor(Color(.primaryNormal))
+                
+                configuration.isOn ? Text("60분")
+                    .bbangFont(BbangzipFont.body4)
+                    .foregroundColor(Color(.staticwhite)) : Text("30분")
+                    .bbangFont(BbangzipFont.body4)
+                    .foregroundColor(Color(.staticwhite))
+            }
+            .padding(.horizontal, 1)
+            
+            HStack(spacing: 0) {
+                
+                if configuration.isOn {
+                    Text("30분")
+                        .bbangFont(BbangzipFont.body4)
+                        .foregroundColor(Color(.primaryNormal))
+                        .frame(width: 48, height: 28)
+                }
+                
+                Spacer()
+                
+                if !configuration.isOn {
+                    Text("60분")
+                        .bbangFont(BbangzipFont.body4)
+                        .foregroundColor(Color(.primaryNormal))
+                        .frame(width: 48, height: 28)
+                }
+
+            }
+            .frame(width: 96, height: 28)
+        }
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: 0.2)) {
+                configuration.isOn.toggle()
+            }
+        }
+    }
+}
+
+struct ToggleButton: View {
+    @State private var isToggleOn = false
+
+    var body: some View {
+        Toggle(isOn: $isToggleOn) { }
+            .toggleStyle(CustomToggleStyle())
+    }
+}
+
+#Preview {
+    ToggleButton()
+}
