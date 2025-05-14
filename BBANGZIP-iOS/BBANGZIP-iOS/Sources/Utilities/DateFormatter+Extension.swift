6500adf59271c49cd0f335bcdfd316896a9b93f8
0gonge <Challengef0802@gmail.com>
2025-05-14 17:23:20 +0900

    feat: DateFormatter+Extension 구현 (#8)

---
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Utilities/DateFormatter+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  DateFormatter+Extension.swift
+//  BBANGZIP
+//
+//  Created by 송여경 on 5/14/25.
+//
+
+import Foundation
+
+extension String {
+    /// "HH:mm" 형식의 24시간 문자열을 "AM/PM h:mm" 형식으로 변환
+    func toAmPmFormattedTime() -> String? {
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "HH:mm"
+        
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateFormat = "a h:mm"
+        outputFormatter.amSymbol = "AM"
+        outputFormatter.pmSymbol = "PM"
+        
+        guard let date = inputFormatter.date(from: self) else { return nil }
+        return outputFormatter.string(from: date)
+    }
+}
