0230a9733adc2ee9f5589934e4c0a20d004312c0
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-05-17 19:40:41 +0900

    feat: Logger Factory 구현 (#20)

---
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Utils/LoggerFactory.swift
@@ -0,0 +1,27 @@
+//
+//  LoggerFactory.swift
+//  BBANGZIP
+//
+//  Created by 조성민 on 5/17/25.
+//
+
+import OSLog
+
+final class LoggerFactory {
+    enum LoggerCategory: String {
+        case presentation
+        case domain
+        case data
+    }
+    
+    static func create(category: LoggerCategory) -> Logger {
+        return Logger(
+            subsystem: String.subsystem,
+            category: category.rawValue
+        )
+    }
+}
+
+extension String {
+    static let subsystem = "BBANGZIP-iOS.app"
+}
