3122655b7f13f2d02f537827866aa98fc8cfbe54
ChoiYouBin <cub8533@naver.com>
2025-05-20 01:50:39 +0900

    feat: Navigation Bar 기본 색 및 아이콘 설정 (#22)

--- BBANGZIP-iOS/BBANGZIP-iOS/Sources/ContentView.swift
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/ContentView.swift
@@ -1,11 +1,38 @@
 import SwiftUI
 
 public struct ContentView: View {
-    public init() {}
+    public init() {
+        UITabBar.appearance().unselectedItemTintColor = UIColor(Color(.labelAssistive))
+    }
 
     public var body: some View {
-        Text("Hello, World!")
-            .padding()
+        TabView {
+            Text("빵굽기")
+                .tabItem {
+                    Image(.icTimer)
+                        .renderingMode(.template)
+                    Text("빵굽기")
+                }
+            Text("할 일")
+                .tabItem {
+                    Image(.icBook)
+                        .renderingMode(.template)
+                    Text("할 일")
+                }
+            Text("이웃")
+                .tabItem {
+                    Image(.icChat)
+                        .renderingMode(.template)
+                    Text("이웃")
+                }
+            Text("마이")
+                .tabItem {
+                    Image(.icPerson)
+                        .renderingMode(.template)
+                    Text("마이")
+                }
+        }
+        .accentColor(Color(.staticblack))
     }
 }
 
