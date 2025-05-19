c4d94cebaa08abdab91c7d9fa1aa58b9c6180250
hongseekim <songhee0617@gmail.com>
2025-05-19 10:46:16 +0900

    chore: 참고용 예시 코드 삭제

--- BBANGZIP-iOS/BBANGZIP-iOS/Sources/Checkbox.swift
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Checkbox.swift
@@ -38,28 +38,3 @@ struct Checkbox: View {
         .buttonStyle(.plain)
     }
 }
-
-//TODO: 참고용 예시 코드
-struct CheckboxPreviewContainer: View {
-    @State var checked1 = false
-    @State var checked2 = false
-    @State var checked3 = false
-    @State var checked4 = false
-    @State var color1 = Color(.todored1)
-    @State var color2 = Color(.todored2)
-    @State var color3 = Color(.todoblue1)
-    @State var color4 = Color(.todoblue2)
-    
-    var body: some View {
-        HStack {
-            Checkbox(isChecked: $checked1, color: $color1)
-            Checkbox(isChecked: $checked2, color: $color2)
-             Checkbox(isChecked: $checked3, color: $color3)
-             Checkbox(isChecked: $checked4, color: $color4)
-        }
-    }
-}
-
-#Preview {
-    CheckboxPreviewContainer()
-}
