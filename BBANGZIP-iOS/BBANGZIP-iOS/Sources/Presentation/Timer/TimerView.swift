886bccf64412f5fd4390b4b13f03ceb060063cf2
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-05-29 21:34:57 +0900

    feat: 타이머 로직 구현 (#25)

---
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Presentation/Timer/TimerView.swift
@@ -0,0 +1,58 @@
+//
+//  TimerView.swift
+//  BBANGZIP
+//
+//  Created by 조성민 on 5/20/25.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    @ObservedObject var viewModel: TimerViewModel
+    
+    var body: some View {
+        VStack {
+            if viewModel.breadCount > 0 {
+                BbangText("\(viewModel.breadCount)", font: .title3)
+            }
+            
+            BbangText(viewModel.announceMessage, font: .title3)
+            
+            BbangText(viewModel.leftTimeText, font: .title2)
+            
+            HStack {
+                if viewModel.state != .initial {
+                    Button {
+                        viewModel.refreshTimer()
+                    } label: {
+                        Image(.icRefreshThin)
+                    }
+                }
+                
+                Button {
+                    viewModel.timerControlButtonTapped()
+                } label: {
+                    Image(viewModel.state == .running ? .icPause : .icStart)
+                }
+                
+                if viewModel.state != .initial {
+                    Button {
+                        viewModel.resetTimer()
+                    } label: {
+                        Image(.icStop)
+                    }
+                }
+            }
+            
+            if viewModel.state == .initial {
+                ToggleButton(isToggleOn: $viewModel.isTimerHour)
+            }
+        }
+    }
+}
+
+#Preview {
+    TimerView(
+        viewModel: TimerViewModel(timerUseCase: TimerUseCaseImpl())
+    )
+}
---
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Presentation/Timer/TimerViewModel.swift
@@ -0,0 +1,105 @@
+//
+//  TimerViewModel.swift
+//  BBANGZIP
+//
+//  Created by 조성민 on 5/20/25.
+//
+
+import SwiftUI
+import Combine
+
+enum TimerState {
+    case initial
+    case running
+    case paused
+    case done
+}
+
+@MainActor
+final class TimerViewModel: ObservableObject {
+    @Published var breadCount: Int = 0
+    @Published var announceMessage: String = "오늘의 빵을 구워보세요!"
+    @Published var leftTimeText: String = "30:00"
+    @Published var leftTimePercentage: CGFloat = 1.0
+    @Published var state: TimerState = .initial
+    @Published var isTimerHour: Bool = false
+    
+    private let timerUseCase: TimerUseCase
+    private var timerTask: Task<Void, Never>?
+    
+    private var leftSeconds: Int = 4
+    private var timeCase: TimerCase = .halfHour
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(timerUseCase: TimerUseCase) {
+        self.timerUseCase = timerUseCase
+        bind()
+    }
+    
+    private func bind() {
+        $isTimerHour
+            .sink { [weak self] isHour in
+                guard let self else { return }
+                timeCase = isHour ? .oneHour : .halfHour
+                leftSeconds = timeCase.totalSeconds
+                leftTimeText = formatTime(seconds: leftSeconds)
+            }
+            .store(in: &cancellables)
+    }
+    
+    func timerControlButtonTapped() {
+        if state == .running {
+            pauseTimer()
+        } else {
+            resumeTimer()
+        }
+    }
+
+    func pauseTimer() {
+        state = .paused
+        announceMessage = "잠시 쉬는 중..."
+        timerUseCase.stop()
+        timerTask?.cancel()
+    }
+
+    func resumeTimer() {
+        state = .running
+        announceMessage = ""
+        timerTask = Task {
+            for await remainingSeconds in timerUseCase.timerStream(from: leftSeconds) {
+                leftSeconds = remainingSeconds
+                leftTimeText = formatTime(seconds: remainingSeconds)
+                leftTimePercentage = CGFloat(remainingSeconds) / CGFloat(timeCase.totalSeconds)
+                print("leftSeconds : \(leftSeconds)")
+            }
+            state = .done
+            announceMessage = "빵이 완성됐어요!"
+        }
+    }
+    
+    func refreshTimer() {
+        timerUseCase.stop()
+        timerTask?.cancel()
+        leftSeconds = timeCase.totalSeconds
+        leftTimeText = formatTime(seconds: leftSeconds)
+        leftTimePercentage = 1.0
+        resumeTimer()
+    }
+
+    func resetTimer() {
+        state = .initial
+        timerUseCase.stop()
+        timerTask?.cancel()
+        leftSeconds = timeCase.totalSeconds
+        leftTimeText = formatTime(seconds: leftSeconds)
+        leftTimePercentage = 1.0
+        announceMessage = "오늘의 빵을 구워보세요!"
+    }
+
+    func formatTime(seconds: Int) -> String {
+        let minutes = seconds / 60
+        let seconds = seconds % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+}
