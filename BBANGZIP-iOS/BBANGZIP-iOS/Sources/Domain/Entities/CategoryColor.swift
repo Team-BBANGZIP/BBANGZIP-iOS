5cb7725a4d30b7609d92de8adf90c63568fe4292
0gonge <Challengef0802@gmail.com>
2025-06-05 14:50:55 +0900

    feat: CategoryColor Enum 정의 (#24)

---
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Domain/Entities/CategoryColor.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryColor.swift
+//  BBANGZIP
+//
+//  Created by 송여경 on 6/5/25.
+//
+
+import Foundation
+
+enum CategoryColor: String, Codable, CaseIterable {
+    case red1
+    case yellow1
+    case green1
+    case blue1
+    case purple1
+    case red2
+    case yellow2
+    case green2
+    case blue2
+    case purple2
+}
