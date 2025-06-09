b0f7ba98339f563a93c951ddea932fdc26736940
0gonge <Challengef0802@gmail.com>
2025-06-09 12:48:40 +0900

    chore: contentView 복구 (#24)

--- BBANGZIP-iOS/BBANGZIP-iOS/Sources/ContentView.swift
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/ContentView.swift
@@ -11,30 +11,29 @@ public struct ContentView: View {
     }
     
     public var body: some View {
-        TabView {
-            CheckedOffView(
-                viewModel: TimerCheckedOffViewModel(
-                    repository: MockTodoRepository(),
-                    toggleUseCase: MockToggleUseCase()
-                )
-            )
-            .tabItem {
-                Image(.icTimer)
-                    .renderingMode(.template)
-                Text("빵굽기")
-            }
+        
+        return TabView {
+            Text("빵 굽기")
+                .tabItem {
+                    Image(.icTimer)
+                        .renderingMode(.template)
+                    Text("빵굽기")
+                }
+            
             Text("할 일")
                 .tabItem {
                     Image(.icBook)
                         .renderingMode(.template)
                     Text("할 일")
                 }
+            
             Text("이웃")
                 .tabItem {
                     Image(.icChat)
                         .renderingMode(.template)
                     Text("이웃")
                 }
+            
             Text("마이")
                 .tabItem {
                     Image(.icPerson)
@@ -49,7 +48,6 @@ public struct ContentView: View {
         )
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
