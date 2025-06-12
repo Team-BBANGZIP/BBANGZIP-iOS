0b2cd4789808c357a2179a52d2494386b797b6a7
hongseekim <songhee0617@gmail.com>
2025-06-13 00:52:22 +0900

    fix: CategoryButton에서 Button 삭제 (#31)

--- BBANGZIP-iOS/BBANGZIP-iOS/Sources/Presentation/Component/CategoryButton.swift
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Presentation/Component/CategoryButton.swift
@@ -10,31 +10,26 @@ import SwiftUI
 struct CategoryButton: View {
     @Binding var color: Color
     @Binding var labelText: String
-    @Binding var isSheetPresented: Bool
     
     var body: some View {
-        Button(action: {
-            isSheetPresented = true
-        }) {
-            HStack(spacing: 6) {
-                Circle()
-                    .fill(color)
-                    .frame(width: 14, height: 14)
-                
-                Text(labelText)
-                    .bbangFont(.label3)
-                    .foregroundColor(Color(.labelStrong))
-                
-                Image(.icPlusThick)
-                    .renderingMode(.template)
-                    .resizable()
-                    .foregroundColor(Color(.labelAlternative))
-                    .frame(width: 18, height: 18)
-            }
-            .padding(.horizontal, 10)
-            .padding(.vertical, 7)
-            .background(Color(.secondaryLight))
-            .cornerRadius(32)
+        HStack(spacing: 6) {
+            Circle()
+                .fill(color)
+                .frame(width: 14, height: 14)
+            
+            Text(labelText)
+                .bbangFont(.label3)
+                .foregroundColor(Color(.labelStrong))
+            
+            Image(.icPlusThick)
+                .renderingMode(.template)
+                .resizable()
+                .foregroundColor(Color(.labelAlternative))
+                .frame(width: 18, height: 18)
         }
+        .padding(.horizontal, 10)
+        .padding(.vertical, 7)
+        .background(Color(.secondaryLight))
+        .cornerRadius(32)
     }
 }
