3deec9a8e25e5f5d23749f83af99bff7f5bbc74b
0gonge <Challengef0802@gmail.com>
2025-09-19 15:23:53 +0900

    feat: 새로운 폰트 시스템 (title4) 적용 (#52)

--- BBANGZIP-iOS/BBANGZIP-iOS/Sources/Font/CustomFont.swift
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Font/CustomFont.swift
@@ -10,7 +10,7 @@ import SwiftUI
 enum BbangzipFont: Sendable {
     case timer
     case picker1, picker2
-    case title1, title2, title3
+    case title1, title2, title3, title4
     case subtitle1, subtitle2, subtitle3
     case body1, body2, body3, body4
     case label1, label2, label3, label4, label5, label6
@@ -25,6 +25,8 @@ enum BbangzipFont: Sendable {
             return 18
         case .title3, .body1:
             return 16
+        case .title4:
+            return 14
         case .subtitle1, .subtitle2, .body2, .label1, .label2:
             return 14
         case .subtitle3, .body4, .label3, .label4:
@@ -49,6 +51,8 @@ enum BbangzipFont: Sendable {
         case .title2:
             return (UIFont(font: BBANGZIPFontFamily.Pretendard.medium, size: size)!.lineHeight - size) * 1.2
         case .title3:
+            return (UIFont(font: BBANGZIPFontFamily.Pretendard.semiBold, size: size)!.lineHeight - size) * 1.4
+        case .title4:
             return (UIFont(font: BBANGZIPFontFamily.Pretendard.semiBold, size: size)!.lineHeight - size) * 1.4
         case .subtitle1:
             return (UIFont(font: BBANGZIPFontFamily.Pretendard.medium, size: size)!.lineHeight - size) * 1.4
@@ -83,7 +87,7 @@ enum BbangzipFont: Sendable {
         switch self {
         case .timer:
             return size * -0.05
-        case .picker1, .picker2, .title1, .title2, .title3,
+        case .picker1, .picker2, .title1, .title2, .title3, .title4,
                 .subtitle1, .subtitle2, .subtitle3, .body1, .body2,
                 .body4, .label1, .label2, .label3, .label4:
             return size * -0.03
@@ -99,7 +103,7 @@ enum BbangzipFont: Sendable {
         case .title2, .subtitle1, .subtitle3, .body1, .body2,
                 .body3, .body4, .label6:
             return BBANGZIPFontFamily.Pretendard.medium.swiftUIFont(size: size)
-        case .title1, .title3, .picker1, .picker2, .label1, .label3, .label5:
+        case .title1, .title3, .title4, .picker1, .picker2, .label1, .label3, .label5:
             return BBANGZIPFontFamily.Pretendard.semiBold.swiftUIFont(size: size)
         case .subtitle2, .label2, .label4:
             return BBANGZIPFontFamily.Pretendard.regular.swiftUIFont(size: size)
