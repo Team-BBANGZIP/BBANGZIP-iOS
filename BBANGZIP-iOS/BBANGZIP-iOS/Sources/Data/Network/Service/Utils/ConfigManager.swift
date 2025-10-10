0f5dfdf89d5884b77f2763569b9c1871c02af8e7
0gonge <Challengef0802@gmail.com>
2025-10-10 22:33:24 +0900

    feat: API에 Interceptor 추가 (#19)

---
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Data/Network/Service/Utils/ConfigManager.swift
@@ -0,0 +1,12 @@
+//
+//  ConfigManager.swift
+//  BBANGZIP
+//
+//  Created by 송여경 on 10/10/25.
+//
+
+import Foundation
+
+enum ConfigManager {
+    static let baseURL: String = Bundle.main.infoDictionary?["BASE_URL"] as! String
+}
