d350d6f8e01b293774e92a40fa08b4458966a050
0gonge <Challengef0802@gmail.com>
2025-11-08 19:02:02 +0900

    feat: SignUp 엔티티 추가 (#66)

---
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Domain/Entities/SignUp.swift
@@ -0,0 +1,16 @@
+//
+//  SignUp.swift
+//  BBANGZIP
+//
+//  Created by 송여경 on 11/8/25.
+//
+
+import Foundation
+
+struct SignUp {
+    let isSuccess: Bool
+    
+    init(isSuccess: Bool) {
+        self.isSuccess = isSuccess
+    }
+}
