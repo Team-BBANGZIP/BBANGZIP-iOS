6f08754894d1e112c24543f0f4509a66f73d630d
0gonge <Challengef0802@gmail.com>
2025-12-18 01:19:13 +0900

    fix: profilekey 추가 DTO 수정 (#79)

--- BBANGZIP-iOS/BBANGZIP-iOS/Sources/Data/Network/Router/DTO/Request/ProfileUpdateRequestDTO.swift
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Data/Network/Router/DTO/Request/ProfileUpdateRequestDTO.swift
@@ -21,5 +21,33 @@ struct ProfileUpdateRequestDTO: Encodable {
         self.nickname = nickname
         self.commitmentMessage = commitmentMessage
     }
-
+    
+    enum CodingKeys: String, CodingKey {
+        case profileImageKey
+        case nickname
+        case commitmentMessage
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encodeIfPresent(profileImageKey, forKey: .profileImageKey)
+        try container.encodeIfPresent(nickname, forKey: .nickname)
+        try container.encodeIfPresent(commitmentMessage, forKey: .commitmentMessage)
+    }
+    
+    func asDictionary() -> [String: Any] {
+        var dict: [String: Any] = [:]
+        
+        if let profileImageKey = profileImageKey {
+            dict["profileImageKey"] = profileImageKey
+        }
+        if let nickname = nickname {
+            dict["nickname"] = nickname
+        }
+        if let commitmentMessage = commitmentMessage {
+            dict["commitmentMessage"] = commitmentMessage
+        }
+        
+        return dict
+    }
 }
--- BBANGZIP-iOS/BBANGZIP-iOS/Sources/Data/Network/Router/DTO/Response/ProfileResponseDTO.swift
+++ BBANGZIP-iOS/BBANGZIP-iOS/Sources/Data/Network/Router/DTO/Response/ProfileResponseDTO.swift
@@ -9,19 +9,38 @@ import Foundation
 
 struct ProfileResponseDTO: Decodable {
     let code: Int
-    let data: ProfileDTO
-    
-    struct ProfileDTO: Decodable {
-        let profileImageUrl: String?
-        let nickname: String
-        let commitmentMessage: String?
-    }
+    let data: ProfileDataDTO
 }
 
-extension ProfileResponseDTO.ProfileDTO {
+struct ProfileDataDTO: Decodable {
+    let profileImageUrl: String?
+    let nickname: String
+    let commitmentMessage: String?
+    
     func toEntity() -> Profile {
+        var extractedKey: Int?
+        
+        if let url = profileImageUrl {
+            let components = url.components(separatedBy: "Profile_")
+            if components.count > 1,
+               let keyString = components.last?.components(separatedBy: ".").first,
+               let key = Int(keyString) {
+                extractedKey = key
+            }
+            
+            if extractedKey == nil {
+                let urlComponents = url.components(separatedBy: "/")
+                if let lastComponent = urlComponents.last,
+                   let keyString = lastComponent.components(separatedBy: ".").first,
+                   let key = Int(keyString) {
+                    extractedKey = key
+                }
+            }
+        }
+        
         return Profile(
             profileImageUrl: profileImageUrl,
+            profileImageKey: extractedKey,
             nickname: nickname,
             commitmentMessage: commitmentMessage
         )
